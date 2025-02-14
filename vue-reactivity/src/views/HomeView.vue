2ed27a6ee5ce4a01e46c255650d2656c069de916
kaisbad <158204041+kaisbad@users.noreply.github.com>
2025-02-14 12:59:16 -0500

    asd

--- vue-reactivity/src/views/HomeView.vue
+++ vue-reactivity/src/views/HomeView.vue
@@ -1,15 +1,18 @@
 //composition api vue v4
 
 <script setup>
-import ShowSpikes from '@/components/ShowSpikes.vue'
 import SpikeDisplay from '@/components/SpikeDisplay.vue'
+import spikes
 </script>
 
 <template>
-  <div class="bg-blue-500">
-    <h1>hello</h1>
-
-    <ShowSpikes />
+  <div class="container flex flex-wrap justify-around w-full mx-auto my-8">
+    <h1>Nike Spikes</h1>
+    <div
+      class="card flex flex-col justify-evenly items-center w-[27%] h-auto border-2 border-black mb-[3%] shadow-[9px_8px_0_black] rounded-2xl"
+      v-for="spike in spikes"
+      :key="spike.name"
+    ></div>
     <SpikeDisplay />
   </div>
 </template>
